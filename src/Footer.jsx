a0a6f53e75f4d029a8c80d3e34b1658973112db1
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-08-21 14:01:13 +0200

    feat: :sparkles: add state

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Navigation from "./Navigation";
 
 export default function Footer(props) {
+  const [showLinks, setShowLinks] = useState(false);
+  const [count, setCount] = useState(0);
+
   const copyrightText = "©Copyright 2023. All rights reserved by Batman.";
   return (
     <footer>
+      <button onClick={() => setShowLinks((prevState) => !prevState)}>
+        Toggle links
+      </button>
+      <h2>Count: {count}</h2>
+      <button
+        onClick={() => {
+          setCount((prevState) => prevState + 1);
+        }}>
+        +
+      </button>
       <p>{copyrightText}</p>
-      <Navigation links={props.links}></Navigation>
+      {showLinks ? <Navigation links={props.links}></Navigation> : null}
     </footer>
   );
 }
