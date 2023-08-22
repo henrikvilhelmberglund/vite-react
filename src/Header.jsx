0d71b6157dbda18959579ca38a6e06de75bc73f3
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-08-22 11:36:11 +0200

    chore: :pencil2: fix typo

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ const educationName = "Frontend Development";
 export default function Header() {
   return (
     <header>
-      <h1>{educationName}</h1>;
+      <h1>{educationName}</h1>
       <h2>
         {name} ({schoolClassName})
       </h2>
